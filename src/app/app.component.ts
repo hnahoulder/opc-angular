554ae7e6346c573d87b532d9b3423ca89901e7b9
houlder.hambalaye <hnahoulder@gmail.com>
2018-06-13 15:59:59 +0200

    Données dynamique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    isAuth = false;
+    appareilOne = 'Machine à laver';
+    appareilTwo = 'Télévision';
+    appareilThree = 'Ordinateur';
+
+    constructor() {
+        setTimeout(() => {
+            this.isAuth = true;
+        }, 3000);
+    }
+
+    onAllumer() {
+        console.log('Allumer tout');
+    }
 }
