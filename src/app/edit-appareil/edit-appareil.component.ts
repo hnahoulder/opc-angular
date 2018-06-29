7ab733b56a6022a86f94bf9133ad0df5c3fba3b5
houlder.hambalaye <hnahoulder@gmail.com>
2018-06-29 17:12:03 +0200

    form

--- src/app/edit-appareil/edit-appareil.component.ts
+++ src/app/edit-appareil/edit-appareil.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
-  selector: 'app-edit-appareil',
-  templateUrl: './edit-appareil.component.html',
-  styleUrls: ['./edit-appareil.component.scss']
+    selector: 'app-edit-appareil',
+    templateUrl: './edit-appareil.component.html',
+    styleUrls: ['./edit-appareil.component.scss']
 })
 export class EditAppareilComponent implements OnInit {
+    defaultOnOff = 'éteint';
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onSubmit(form: NgForm) {
+        const name = form.value['name'];
+        const status = form.value['status'];
+    }
 }
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -41,4 +41,16 @@ export class AppareilService {
     switchOffOne(index: number) {
         this.appareils[index].status = 'éteint';
     }
+
+    addAppareil(name: string, status: string) {
+        const appareilObject = {
+            id: 0,
+            name: '',
+            status: ''
+        };
+        appareilObject.name = name;
+        appareilObject.status = status;
+        appareilObject.id = this.appareils[(this.appareils.length - 1)].id = 1;
+        this.appareils.push(appareilObject);
+    }
 }
